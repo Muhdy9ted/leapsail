6a640ba7c84439300ca916be78be3f5c546adb3e
Muhammed Abdullahi <MABDULLAHI@unionbankng.com>
2022-08-11 12:57:30 +0100

    finished implementing the navigation bar

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import router from './router'
+import styles from './assets/styles/styles.scss'
 
 Vue.config.productionTip = false
+Vue.use(styles)
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  router,
+  render: h => h(App)
 }).$mount('#app')
